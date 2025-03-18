8484444dda614b7d68ae1060dc570f575a514348
Ram Sharan Rimal <rsr0012@uah.edu>
2025-03-18 15:07:23 -0500

    add toggle component

---
+++ src/components/ui/toggle/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const ToggleSwitch = ({ title, onToggle }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleToggle = () => {
+    const newState = !isChecked;
+    setIsChecked(newState);
+    if (onToggle) {
+      onToggle(newState); // Callback with the updated state
+    }
+  };
+
+  return (
+    <div className='toggle-container'>
+      <span className='custom-label-cov'> {title}</span>
+      <label className='toggle-switch'>
+        <input
+          type='checkbox'
+          className='toggle-input'
+          id='showCoverage'
+          checked={isChecked}
+          onChange={handleToggle}
+        />
+        <span className='slider'></span>
+      </label>
+    </div>
+  );
+};
+
+export default ToggleSwitch;
