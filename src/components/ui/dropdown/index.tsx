c6483068b7b02978102646f9984c644d2fcafc7a
Sanjog Thapa <sanzog03@gmail.com>
2025-07-22 17:37:31 -0500

    1. titlize the dropdown options 2. caption over the dropdown

--- src/components/ui/dropdown/index.tsx
+++ src/components/ui/dropdown/index.tsx
@@ -3,6 +3,7 @@ import {
   Select,
   MenuItem,
   SelectChangeEvent,
+  InputLabel,
 } from '@mui/material';
 import { Box } from '@mui/system';
 
@@ -28,20 +29,39 @@ export function Dropdown({
   return (
     <Box id='dropdown' sx={{ minWidth: 120, maxWidth: 240 }} style={style}>
       <FormControl fullWidth>
+        <InputLabel id='demo-simple-select-label'>Target Type</InputLabel>
         <Select
           labelId=''
-          id='target-types-dropdown'
+          id='demo-simple-select'
           value={selectedItemId}
           onChange={handleChange}
-          inputProps={{ 'aria-label': 'Without label' }}
+          // inputProps={{ 'aria-label': 'Without label' }}
+          label='Target Type'
         >
           {items.map((item) => (
             <MenuItem key={item} value={item}>
-              {item}
+              {titleCase(item)}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
     </Box>
   );
+}
+
+function titleCase(text: string) {
+  if (!text) {
+    return ''; // Handle empty strings gracefully
+  }
+
+  // Convert the string to lowercase, then split it into an array of words
+  const words = text.toLowerCase().split(' ');
+
+  // Map over the array, capitalizing the first letter of each word
+  const titleCasedWords = words.map((word: string) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  // Join the words back into a single string with spaces
+  return titleCasedWords.join(' ');
 }
